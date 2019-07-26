3fc1d876564c1adbe06c147d4afe04b80e37c744
Jess Rezac <jess.rezac@gmail.com>
2019-07-26 17:39:28 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,10 +2,10 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
-  
+
   def actors_list
     self.actors.map do |actor|
-      self.full_name
+      actor.full_name
     end
   end
 end
