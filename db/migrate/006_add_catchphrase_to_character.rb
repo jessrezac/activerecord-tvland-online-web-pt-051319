26cfb5ebbfbd5674bca43fda2ecd6f2b929120f6
Jess Rezac <jess.rezac@gmail.com>
2019-07-26 17:25:42 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_character.rb
+++ db/migrate/006_add_catchphrase_to_character.rb
@@ -1,5 +1,5 @@
 class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
   def change
-    add_column :catchphrase, :characters, :text
+    add_column :catchphrase, :characters, :string
   end
 end
